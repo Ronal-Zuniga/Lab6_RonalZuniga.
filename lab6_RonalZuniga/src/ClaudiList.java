86285e2710854b9b6dc97511549da0b816042da1
Ronal-Zuniga <69054954+Ronal-Zuniga@users.noreply.github.com>
2020-11-27 14:50:05 -0600

    Clases, CRUD programas

---
+++ lab6_RonalZuniga/src/ClaudiList.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+
+public class ClaudiList {
+    private String nombre;
+    private ArrayList<Programa> programas = new ArrayList();
+
+    public ClaudiList() {
+    }
+
+    public ClaudiList(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Programa> getProgramas() {
+        return programas;
+    }
+
+    public void setProgramas(ArrayList<Programa> programas) {
+        this.programas = programas;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+}
